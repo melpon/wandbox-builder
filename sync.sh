34e53484df556c71f20feb5f7ddfd2a6f10fe0ea
melpon <shigemasa7watanabe+github@gmail.com>
2017-01-26 01:54:58 +0900

    fix copy directory

--- sync.sh
+++ sync.sh
@@ -10,6 +10,6 @@ if [ $# -lt 1 ]; then
 fi
 
 BASE_DIR=$(cd $(dirname $0); pwd)
-rsync -rlpt --delete -v $BASE_DIR/wandbox $1:/opt/wandbox
+rsync -rlpt --delete -v $BASE_DIR/wandbox $1:/opt/
 # TODO: cattleshedのためにこれを実行する必要がある（要root権限）
 # ssh $1 setcap cap_sys_admin,cap_sys_chroot,cap_mknod,cap_net_admin=p /opt/wandbox/cattleshed/bin/cattlegrid
