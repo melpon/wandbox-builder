b9d1a47d52f06d7214de31d46bcb87a512726af2
melpon <shigemasa7watanabe+github@gmail.com>
2017-01-28 22:44:03 +0900

    add header only libraries for C++

--- build-head.sh
+++ build-head.sh
@@ -24,12 +24,32 @@ for compiler in \
   fi
 done
 
-./docker-rm.sh
-
-cd ..
-
 # copy static files
 mkdir wandbox/static || true
 cp -r static/ wandbox/static/
+
+# header only library
+rm -rf wandbox/sprout || true
+git clone --depth 1 https://github.com/bolero-MURAKAMI/Sprout.git wandbox/sprout
+rm -rf wandbox/range-v3 || true
+git clone --depth 1 https://github.com/ericniebler/range-v3.git wandbox/range-v3
+rm -rf wandbox/boost-sml || true
+git clone --depth 1 https://github.com/boost-experimental/sml.git wandbox/boost-sml
+rm -rf wandbox/msgpack-c || true
+git clone --depth 1 https://github.com/msgpack/msgpack-c.git wandbox/msgpack-c
+
+# change owner to root
+docker run --net=host -i -v $BASE_DIR/../wandbox:/opt/wandbox ubuntu:16.04 /bin/bash -c "
+  set -ex
+  chown -R root:root /opt/wandbox/static
+  chown -R root:root /opt/wandbox/sprout
+  chown -R root:root /opt/wandbox/range-v3
+  chown -R root:root /opt/wandbox/boost-sml
+  chown -R root:root /opt/wandbox/msgpack-c
+"
+
+./docker-rm.sh
+
+cd ..
 
 ./sync.sh cattleshed-root
