1645ccdec1abc6e4592e2e1633fd1a90b0bc6e10
melpon <shigemasa7watanabe+github@gmail.com>
2017-01-31 04:34:45 +0900

    implement cattleshed test tool.

---
+++ test/run-test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+BASE_DIR=$(cd $(dirname $0); pwd)
+
+if [ $# -lt 1 ]; then
+    echo "usage:"
+    echo "  $0 test1 [test2 [test3 [...]]]"
+    echo "  $0 --all"
+    echo ""
+    echo "example:"
+    echo "  $0 'gcc-head'"
+    echo "  $0 'clang-*'"
+    echo "  $0 'boost-*-gcc-*' 'boost-1.6?.0-clang-*'"
+    exit 0
+fi
+
+../build/docker-rm.sh > /dev/null 2>&1
+
+# run test-server
+docker run \
+  --name test-server \
+  -v `pwd`:/var/work \
+  -v $BASE_DIR/../wandbox:/opt/wandbox \
+  -d \
+  --privileged=true \
+  melpon/wandbox:test-server \
+  /bin/bash -c "
+    set -e
+
+    mkdir /usr/share/perl || true
+    /opt/wandbox/cattleshed/bin/cattleshed \
+      -c /opt/wandbox/cattleshed/etc/cattleshed.conf \
+      -c /opt/wandbox/cattleshed-conf/compilers.default &
+    sleep 1
+    /opt/wandbox/kennel/bin/kennel \
+      -c /var/work/kennel.json &
+
+    # wait forever
+    while true; do
+      sleep 10
+    done
+    "
+
+# run test-client
+docker run \
+  --name test-client \
+  --link test-server:test-server \
+  -v `pwd`/..:/var/work \
+  -v $BASE_DIR/../wandbox:/opt/wandbox \
+  -it \
+  melpon/wandbox:test-client \
+  /bin/bash -c "
+    cd /var/work/test
+    python run.py $@
+  "
+
+# stop test-server
+docker stop test-server
+
+../build/docker-rm.sh
