c56dc379394a2b3e7275e1932bb377eed20510cf
melpon <shigemasa7watanabe+github@gmail.com>
2017-02-25 01:57:41 +0900

    fix pypy-head

--- build/pypy-head/install.sh
+++ build/pypy-head/install.sh
@@ -20,8 +20,8 @@ cp -r pypy $PREFIX
 
 # version
 
-PYPY_VERSION=`/opt/wandbox/pypy-$VERSION/bin/pypy --version 2>&1 | tail -n 1 | cut -d' ' -f2`
-CPYTHON_VERSION=`/opt/wandbox/pypy-$VERSION/bin/pypy --version 2>&1 | head -n 1 | cut -d' ' -f2`
+PYPY_VERSION=`/opt/wandbox/pypy-head/bin/pypy --version 2>&1 | tail -n 1 | cut -d' ' -f2`
+CPYTHON_VERSION=`/opt/wandbox/pypy-head/bin/pypy --version 2>&1 | head -n 1 | cut -d' ' -f2`
 
 echo "$PYPY_VERSION cpython-$CPYTHON_VERSION" > $PREFIX/VERSION
 
