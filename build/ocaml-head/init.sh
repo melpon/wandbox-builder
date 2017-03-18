b4dd76785415427d5fe38f4101a550158b580158
melpon <shigemasa7watanabe+github@gmail.com>
2017-03-19 01:26:59 +0900

    fix ocaml-head

--- build/ocaml-head/init.sh
+++ build/ocaml-head/init.sh
@@ -5,5 +5,5 @@
 function test_ocaml() {
   prefix=$1
   cd $BASE_DIR/resources
-  $prefix/bin/with-env.sh ocamlfind ocamlopt -linkpkg core -thread test.ml -o test && test "`./test`" = "Hello, world!" && rm test && rm test.cmi && rm test.cmo && rm test.cmi
+  $prefix/bin/with-env.sh ocamlfind ocamlopt -linkpkg -thread test.ml -o test && test "`./test`" = "Hello, world!" && rm test && rm test.cmi && rm test.cmo && rm test.cmi
 }
