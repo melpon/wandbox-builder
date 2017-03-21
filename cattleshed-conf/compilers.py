449216107795a776d10a9515ade27d6d2da35e67
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-03-21 20:22:45 +0900

    pony: move temporary project directory into /tmp

--- cattleshed-conf/compilers.py
+++ cattleshed-conf/compilers.py
@@ -1806,7 +1806,7 @@ class Compilers(object):
                 'language': 'Pony',
                 'output-file': 'prog.pony',
                 'compiler-option-raw': True,
-                'compile-command': ['/bin/bash', '-c', "mkdir -p prog && cp ./prog.pony ./prog/main.pony && /opt/wandbox/pony-{cv}/bin/ponyc ./prog"],
+                'compile-command': ['/bin/bash', '-c', "mkdir -p /tmp/prog && cp ./prog.pony /tmp/prog/main.pony && /opt/wandbox/pony-{cv}/bin/ponyc /tmp/prog"],
                 'version-command': version_command,
                 'switches': [],
                 'initial-checked': [],
