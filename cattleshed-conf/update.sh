73959b93310db1ee7ec71f0cc321a60f81e835bd
melpon <shigemasa7watanabe+github@gmail.com>
2017-03-23 00:26:14 +0900

    generate cattleshed-conf directory

--- cattleshed-conf/update.sh
+++ cattleshed-conf/update.sh
@@ -3,5 +3,6 @@
 # change owner to root
 docker run --net=host -i -v `pwd`/..:/var/work -v `pwd`/../wandbox:/opt/wandbox melpon/wandbox:cattleshed-conf /bin/bash -c "
   cd /var/work/cattleshed-conf
+  mkdir -p /opt/wandbox/cattleshed-conf
   python compilers.py > /opt/wandbox/cattleshed-conf/compilers.default
 "
