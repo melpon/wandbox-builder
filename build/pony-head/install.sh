1599fec760ff57fd4b9f99232216912dc7f1a72d
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2017-03-24 08:03:16 +0900

    pony: install LLVM at creation Docker image

--- build/pony-head/install.sh
+++ build/pony-head/install.sh
@@ -3,18 +3,8 @@
 . ../init.sh
 
 PREFIX=/opt/wandbox/pony-head
-LLVM_VERSION=3.9.1
 
 cd ~/
-
-# install prebuilt LLVM
-wget_strict_sha256 \
-  "http://releases.llvm.org/${LLVM_VERSION}/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-16.04.tar.xz" \
-  "$BASE_DIR/resources/clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-16.04.tar.xz.sha256"
-tar xJf "clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-16.04.tar.xz" \
-  --strip-components 1 \
-  -C /usr/local/ \
-  "clang+llvm-${LLVM_VERSION}-x86_64-linux-gnu-ubuntu-16.04"
 
 # get sources
 git clone --depth 1 https://github.com/ponylang/ponyc.git
