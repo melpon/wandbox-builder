86ce0c9620d85a5a5e33ac5fbfc91a72760eb1f2
melpon <shigemasa7watanabe+github@gmail.com>
2017-03-24 21:42:42 +0900

    fix head error

--- build-head.sh
+++ build-head.sh
@@ -73,7 +73,7 @@ for compiler in \
     pony-head \
 ; do
   run $compiler > $LOG_DIR/$compiler.log 2>&1 || echo "$compiler: $?" >> $LOG_DIR/failed.log
-  ./build/docker-rm.sh
+  $BASE_DIR/build/docker-rm.sh
 done
 
 cd ..
