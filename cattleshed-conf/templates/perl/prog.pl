6ee8f53200321dc762b46ed9677b895eadbd7e42
melpon <shigemasa7watanabe+github@gmail.com>
2017-04-04 07:26:13 +0900

    fix https://github.com/melpon/wandbox/issues/208

--- cattleshed-conf/templates/perl/prog.pl
+++ cattleshed-conf/templates/perl/prog.pl
@@ -1,6 +1,6 @@
-// This file is a "Hello, world!" in Perl language for wandbox.
+# This file is a "Hello, world!" in Perl language for wandbox.
 
 print "Hello, Wandbox!\n";
 
-// Perl language references:
-//   http://perldoc.perl.org/
+# Perl language references:
+#   http://perldoc.perl.org/
