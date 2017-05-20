a315ed6369614d39391f39980a2f9bd414d23917
kenji yoshida <6b656e6a69@gmail.com>
2017-05-20 22:24:51 +0900

    fix scala template
    - change space 2 indent http://docs.scala-lang.org/style/indentation.html
    - fix procedure syntax https://github.com/scala/bug/issues/7605

--- cattleshed-conf/templates/scala/prog.scala
+++ cattleshed-conf/templates/scala/prog.scala
@@ -1,11 +1,9 @@
 // This file is a "Hello, world!" in Scala language for wandbox.
 
-object Wandbox
-{
-    def main(args: Array[String])
-    {
-        println("Hello, Wandbox!")
-    }
+object Wandbox {
+  def main(args: Array[String]): Unit = {
+    println("Hello, Wandbox!")
+  }
 }
 
 // Scala language references:
