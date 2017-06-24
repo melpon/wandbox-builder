83c0c52e76556f518f4f709179180b3e31453764
melpon <shigemasa7watanabe+github@gmail.com>
2017-06-24 16:27:52 +0900

    add coffeescript-1.12.6

--- build/coffeescript/install.sh
+++ build/coffeescript/install.sh
@@ -14,14 +14,15 @@ NODEJS_PREFIX=/opt/wandbox/nodejs-6.9.5
 # get sources
 
 cd ~/
-git clone --depth 1 --branch 1.12.3 https://github.com/jashkenas/coffeescript.git
+git clone --depth 1 --branch $VERSION https://github.com/jashkenas/coffeescript.git
 cd coffeescript
 
 # build
 
 PATH=$NODEJS_PREFIX/bin:$PATH npm update
-PATH=$NODEJS_PREFIX/bin:$PATH node bin/cake --prefix $PREFIX install
+PATH=$NODEJS_PREFIX/bin:$PATH npm install --prefix $PREFIX
 
+ln -s $PREFIX/node_modules/.bin $PREFIX/bin
 cp $BASE_DIR/resources/run-coffee.sh.in $PREFIX/bin/run-coffee.sh
 sed -i "s#@nodejs_prefix@#$NODEJS_PREFIX#g" $PREFIX/bin/run-coffee.sh
 sed -i "s#@prefix@#$PREFIX#g" $PREFIX/bin/run-coffee.sh
