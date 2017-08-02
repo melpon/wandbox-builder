8929b489f1f30dee4bc0173e515fee3a2fe249f1
melpon <shigemasa7watanabe+github@gmail.com>
2017-08-02 23:22:19 +0900

    fix ghc-head

--- build/ghc-head/install.sh
+++ build/ghc-head/install.sh
@@ -3,6 +3,7 @@
 . ../init.sh
 
 PREFIX=/opt/wandbox/ghc-head
+WITH_GHC=/opt/wandbox/ghc-8.0.2/bin/ghc
 
 # get sources
 
@@ -12,11 +13,11 @@ git clone --recursive git://git.haskell.org/ghc.git
 # install
 
 cd ghc
-./boot
-./configure --prefix=$PREFIX # --with-ghc=...
 cp mk/build.mk.sample mk/build.mk
 sed -i 's/#BuildFlavour = devel2/BuildFlavour = devel2/' mk/build.mk
-make
+./boot
+./configure --prefix=$PREFIX --with-ghc=$WITH_GHC
+make # -j2 <- insufficient memory
 rm -r $PREFIX/*
 make install
 rm -r $PREFIX/share
