9c7fee2299ddc404e74ab042e60b0794050a7085
melpon <shigemasa7watanabe+github@gmail.com>
2017-09-06 22:43:56 +0900

    add coffeescript-1.12.7

--- build/coffeescript/install.sh
+++ build/coffeescript/install.sh
@@ -20,9 +20,9 @@ cd coffeescript
 # build
 
 PATH=$NODEJS_PREFIX/bin:$PATH npm update
-PATH=$NODEJS_PREFIX/bin:$PATH npm install --prefix $PREFIX
+PATH=$NODEJS_PREFIX/bin:$PATH npm -g set prefix $PREFIX
+PATH=$NODEJS_PREFIX/bin:$PATH npm -g install
 
-ln -s $PREFIX/node_modules/.bin $PREFIX/bin
 cp $BASE_DIR/resources/run-coffee.sh.in $PREFIX/bin/run-coffee.sh
 sed -i "s#@nodejs_prefix@#$NODEJS_PREFIX#g" $PREFIX/bin/run-coffee.sh
 sed -i "s#@prefix@#$PREFIX#g" $PREFIX/bin/run-coffee.sh
