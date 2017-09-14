8efddc7e80a5787762c65cab34e6e252bbdb0ef8
melpon <shigemasa7watanabe+github@gmail.com>
2017-09-15 00:32:20 +0900

    add ghc-8.2.1

--- build/ghc/install.sh
+++ build/ghc/install.sh
@@ -15,6 +15,7 @@ case $VERSION in
   "7.8.3" ) URL="https://www.haskell.org/ghc/dist/7.8.3/ghc-7.8.3-x86_64-unknown-linux-deb7.tar.bz2" ;;
   "7.10.3" ) URL="http://downloads.haskell.org/~ghc/7.10.3/ghc-7.10.3b-x86_64-deb8-linux.tar.bz2" ;;
   "8.0.2" ) URL="http://downloads.haskell.org/~ghc/8.0.2/ghc-8.0.2-x86_64-deb8-linux.tar.xz" ;;
+  "8.2.1" ) URL="https://downloads.haskell.org/~ghc/8.2.1/ghc-8.2.1-x86_64-deb8-linux.tar.xz" ;;
   * ) exit 1 ;;
 esac
 
