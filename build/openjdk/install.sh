8d39d6d0f1b1df4f8488bf32054f281a776addb1
melpon <shigemasa7watanabe+github@gmail.com>
2017-10-31 12:02:39 +0900

    specify OpenJDK version string with configure

--- build/openjdk/install.sh
+++ build/openjdk/install.sh
@@ -17,11 +17,33 @@ PREFIX=/opt/wandbox/openjdk-$VERSION
 # get sources
 
 case "$VERSION" in
-  "jdk-10+13" ) URL=http://hg.openjdk.java.net/jdk10/jdk10 ;;
-  "jdk-9+175" ) URL=http://hg.openjdk.java.net/jdk9/jdk9 ;;
-  "jdk-9+155" ) URL=http://hg.openjdk.java.net/jdk9/jdk9 ;;
-  "jdk8u121-b13" ) URL=http://hg.openjdk.java.net/jdk8u/jdk8u ;;
-  "jdk7u121-b00" ) URL=http://hg.openjdk.java.net/jdk7u/jdk7u ;;
+  "jdk-10+23" )
+    VERSION_FLAGS="--with-version-string=10+23"
+    URL=http://hg.openjdk.java.net/jdk10/jdk10 ;;
+  "jdk-10+13" )
+    VERSION_FLAGS="--with-version-string=10+13"
+    URL=http://hg.openjdk.java.net/jdk10/jdk10 ;;
+  "jdk-9+181" )
+    VERSION_FLAGS="--with-version-string=9+181"
+    URL=http://hg.openjdk.java.net/jdk9/jdk9 ;;
+  "jdk-9+175" )
+    VERSION_FLAGS="--with-version-string=9+175"
+    URL=http://hg.openjdk.java.net/jdk9/jdk9 ;;
+  "jdk-9+155" )
+    VERSION_FLAGS="--with-version-string=9+155"
+    URL=http://hg.openjdk.java.net/jdk9/jdk9 ;;
+  "jdk8u121-b13" )
+    VERSION_FLAGS="--with-update-version=121 --with-build-number=b13 --with-milestone=fcs"
+    URL=http://hg.openjdk.java.net/jdk8u/jdk8u ;;
+  "jdk7u121-b00" )
+    export JDK_VERSION="1.7.0_121"
+    export JDK_MAJOR_VERSION="1"
+    export JDK_MINOR_VERSION="7"
+    export JDK_MICRO_VERSION="0_121"
+    export MILESTONE="fcs"
+    export BUILD_NUMBER="b00"
+    export FULL_VERSION="1.7.0_121-b00"
+    URL=http://hg.openjdk.java.net/jdk7u/jdk7u ;;
   * ) exit 1 ;;
 esac
 
@@ -45,8 +67,9 @@ if [ "$VERSION" = "jdk7u121-b00" ]; then
   rm -r $PREFIX || true
   cp -r build/linux-amd64 $PREFIX
 else
-  bash configure --prefix=$PREFIX --with-memory-size=2048 --with-num-cores=3
+  bash configure --prefix=$PREFIX --with-memory-size=2048 --with-num-cores=3 $VERSION_FLAGS
   make all
+  rm -r $PREFIX || true
   make install
 fi
 
