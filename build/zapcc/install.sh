5f873e0e67b92b0a9aee1612f04461e05a7604f9
melpon <shigemasa7watanabe+github@gmail.com>
2017-11-14 22:41:30 +0900

    add zapcc-2017.08

--- build/zapcc/install.sh
+++ build/zapcc/install.sh
@@ -10,13 +10,26 @@ fi
 VERSION=$1
 PREFIX=/opt/wandbox/zapcc-$VERSION
 
+case "$VERSION" in
+  "1.0.1" ) ;;
+  "2017.08" ) RESOURCE_NAME="20170802-175507" ;;
+  * ) exit 1 ;;
+esac
+
 # extract
 
 cd ~/
-mkdir zapcc-$VERSION
-cd zapcc-$VERSION
-tar xf $BASE_DIR/resources/zapcc-$VERSION.tar.gz --strip-component=1
-cd ..
+
+if [ "$VERSION" = "1.0.1" ]; then
+  mkdir zapcc-$VERSION
+  cd zapcc-$VERSION
+  tar xf $BASE_DIR/resources/zapcc-$VERSION.tar.gz --strip-component=1
+  cd ..
+else
+  cp $BASE_DIR/resources/zapcc-$RESOURCE_NAME.7z ./
+  7zr x zapcc-$RESOURCE_NAME.7z
+  mv zapcc-$RESOURCE_NAME zapcc-$VERSION
+fi
 
 # install
 
@@ -25,4 +38,4 @@ cp -r ~/zapcc-$VERSION $PREFIX
 
 # add license
 
-#cp $BASE_DIR/resources/zapcc-key.txt $PREFIX/bin
+cp $BASE_DIR/resources/zapcc-key.txt $PREFIX/bin
