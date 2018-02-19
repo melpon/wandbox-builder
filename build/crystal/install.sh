1d18e651871a18bead45eb02b9d4356f1fdb3170
nekketsuuu <nekketsuuu@users.noreply.github.com>
2018-02-19 18:13:54 +0900

    fix bootstrapping errors for crystal <=0.23.1 (#17)

--- build/crystal/install.sh
+++ build/crystal/install.sh
@@ -10,6 +10,21 @@ fi
 VERSION="$1"
 PREFIX="/opt/wandbox/crystal-$VERSION"
 
+cd /usr/local/
+
+# prepare for bootstrapping
+if compare_version "$VERSION" "<=" "0.23.1" ; then
+  # Crystal 0.23.1 can't be compiled by Crystal >= 0.24.0 (melpon/wandbox-builder#17)
+  archive="crystal-0.23.1-3-linux-x86_64.tar.gz"
+  wget_strict_sha256 \
+    "https://github.com/crystal-lang/crystal/releases/download/0.23.1/${archive}" \
+    "$BASE_DIR/resources/${archive}.sha256"
+  tar xf "./${archive}"
+  rm "./${archive}"
+  export PATH="/usr/local/crystal-0.23.1-3/bin/:$PATH"
+fi
+crystal --version
+
 cd ~/
 
 # get source
