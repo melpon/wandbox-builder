53e4b4c0ff42dcb3500bffc0fc0d8103c3096113
melpon <shigemasa7watanabe+github@gmail.com>
2018-09-05 00:46:29 +0900

    fix R template for https://github.com/melpon/wandbox/issues/243#issuecomment-418403041

--- cattleshed-conf/templates/r/prog.R
+++ cattleshed-conf/templates/r/prog.R
@@ -1,9 +1,10 @@
 # This file is a "Hello, world!" in R language for wandbox.
 
-data(iris)      ## the "hello, world" of statistics.
-summary(iris)   ## a simple summary of the columns
-table( iris[, "Species"] )   # tabulation of the Species columns
-stopifnot( as.integer(table(iris[, "Species"])) == c(50L, 50L, 50L) )
+data(iris)                               ## the "hello, world" of statistics.
+summary(iris)                            ## a simple summary of the columns
+fit <- lm(Sepal.Length ~ . , data=iris)  ## basic regression
+summary(fit)                             ## summary
+
 cat("All done\n")
 
 # R language references:
