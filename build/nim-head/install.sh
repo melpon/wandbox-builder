a74c570b03293a858f5b3d9f2b182431ed283e81
demotomohiro <gpuppur@gmail.com>
2019-01-22 10:59:39 +0900

    nim-head download source from official nighty

--- build/nim-head/install.sh
+++ build/nim-head/install.sh
@@ -6,13 +6,20 @@ PREFIX=/opt/wandbox/nim-head
 
 cd ~/
 
-# get sources
-git clone --depth 1 https://github.com/nim-lang/Nim.git
-cd Nim
-# clones `csources.git`, bootstraps Nim compiler and compiles tools
-sh build_all.sh
+# Download nightly rather than source with git clone.
+# Because nightles are posted only when it passes all testing.
+curl -s https://api.github.com/repos/nim-lang/nightlies/releases/latest | \
+  grep "\"browser_download_url\": \".*-linux\.tar\.xz\"" | \
+  sed -E 's/.*"browser_download_url\": \"([^"]+)\"/\1/' | \
+  wget -qi - -O nim.tar.xz
+tar xf nim.tar.xz
+cd nim*
+# bootstraps Nim compiler and compiles tools
+sh build.sh
+bin/nim c koch
+./koch tools
 
 # install
-./koch install /opt
+sh install.sh ~/tmp
 [ -e "$PREFIX" ] && rm -r "$PREFIX"
-mv /opt/nim "$PREFIX"
+mv ~/tmp "$PREFIX"
