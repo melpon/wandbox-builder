cd18da6f6fc64c30f6a97ae25c756b2f2e07d002
happou31 <happo31@gmail.com>
2019-05-30 03:00:21 +0900

    fix: for typescript

--- build/typescript/test.sh
+++ build/typescript/test.sh
@@ -8,6 +8,7 @@ if [ $# -lt 1 ]; then
 fi
 
 VERSION=$1
-PREFIX=/opt/wandbox/nodejs-$VERSION
+PREFIX=/opt/wandbox/typescript-$VERSION
 
-test "`$PREFIX/bin/node $BASE_DIR/resources/test.js`" = "hello"
+test "`$PREFIX/node_modules/typescriptbin/tsc $BASE_DIR/resources/test.ts` && node $BASE_DIR/resources/test.js" = "hello"
+rm $BASE_DIR/resources/test.js
