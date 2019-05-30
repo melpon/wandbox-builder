0ef9efcf967d6dc8741e32ec138f5109f4320d3e
happou31 <happo31@gmail.com>
2019-05-30 18:34:42 +0900

    add: nodejs build and install

--- build/typescript/install.sh
+++ build/typescript/install.sh
@@ -9,6 +9,7 @@ fi
 
 VERSION=$1
 PREFIX=/opt/wandbox/typescript-$VERSION
+NODE_VERSION=10.16.0
 
 # create dir
 
@@ -19,7 +20,22 @@ cd $PREFIX
 
 echo "{ \"dependencies\": { \"typescript\": \"$VERSION\" } }" > package.json
 
-# install
+# install nodejs
+
+wget_strict_sha256 \
+  https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.gz \
+  $BASE_DIR/resources/node-v$NODE_VERSION/node-v$NODE_VERSION.tar.gz.sha256
+
+tar xf node-v$NODE_VERSION.tar.gz
+cd node-v$NODE_VERSION
+
+# build nodejs
+
+./configure --prefix=$PREFIX --partly-static
+make -j2
+make install
+
+PATH=$PREFIX/bin:$PATH npm update -g
 
 npm install
 rm package.json
