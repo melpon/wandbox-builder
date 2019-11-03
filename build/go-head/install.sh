53ddcfc14af917f9cbd1893d5d3f6a352a3897fd
melpon <shigemasa7watanabe+github@gmail.com>
2019-11-03 09:50:34 +0900

    golang で不要らしいファイルを削除

--- build/go-head/install.sh
+++ build/go-head/install.sh
@@ -17,3 +17,7 @@ mv go $PREFIX
 cd $PREFIX/src
 
 ./make.bash
+
+# https://github.com/docker-library/golang/blob/f30f0428221b94c7dcb414ebdcea83106df20897/1.13/alpine3.10/Dockerfile#L47-L53
+rm -rf $PREFIX/pkg/bootstrap
+rm -rf $PREFIX/pkg/obj
