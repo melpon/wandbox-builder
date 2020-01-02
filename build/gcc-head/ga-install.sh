d798ff4a0f61c0612881bbdc4a8284966d32ed9c
melpon <shigemasa7watanabe+github@gmail.com>
2020-01-03 04:44:22 +0900

    追加忘れてた

---
+++ build/gcc-head/ga-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y \
+  bison \
+  build-essential \
+  curl \
+  flex \
+  git \
+  libc6-dev-i386 \
+  libgmp-dev \
+  libmpc-dev \
+  libmpfr-dev \
+  libtool \
+  m4 \
+  realpath \
+  unzip \
+  wget
+
+./install.sh
