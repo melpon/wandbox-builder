d71a4df83a3b815aa8a3ca93f51710352b227c4f
melpon <shigemasa7watanabe+github@gmail.com>
2020-01-05 02:39:45 +0900

    test

--- build/openjdk-head/ga-install.sh
+++ build/openjdk-head/ga-install.sh
@@ -31,6 +31,12 @@ apt-get install -y \
   unzip \
   zip
 
+echo $JAVA_HOME
+
+update-alternatives --config java -l
+update-alternatives --config java
+update-alternatives --config java -s java-1.8.0-openjdk-amd64
+
 # OpenJDK 10 からビルド
 pushd ../openjdk
   ./install.sh jdk-10+23
