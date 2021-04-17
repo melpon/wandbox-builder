eb4486f1d553551dff27c3f48814c7e956bb33e5
melpon <shigemasa7watanabe+github@gmail.com>
2021-04-17 23:02:07 +0900

    mono 追加

---
+++ ga-build/mono/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+BASE_DIR=$(cd $(dirname $0); pwd)
+cd $BASE_DIR
+
+. ../init-common.sh
+
+if [ "$SUBCOMMAND" == "setup" ]; then
+  sudo apt-get install -y \
+    mono-devel \
+    python
+  exit 0
+fi
+
+EXT=xz
+if compare_version "$VERSION" "<" "6.0.0"; then
+  EXT=bz2
+fi
+
+wget_strict_sha256 \
+  https://download.mono-project.com/sources/mono/mono-$VERSION.tar.$EXT \
+  $BASE_DIR/resources/mono-$VERSION.tar.$EXT.sha256
+
+mkdir mono-$VERSION
+pushd mono-$VERSION
+  tar xf ../mono-$VERSION.tar.$EXT --strip-component 1
+
+  # build
+  export CC="gcc -static-libgcc -static-libstdc++"
+  export CXX="g++ -static-libgcc -static-libstdc++"
+  ./configure --prefix=$PREFIX --disable-nls --disable-quiet-build --disable-system-aot
+  make -j`nproc`
+  make install
+popd
+
+archive_install $PREFIX $PACKAGE_PATH $PACKAGE_FILENAME
