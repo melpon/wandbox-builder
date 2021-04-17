cf29170f4ac13bb9b9c489f91e66a4a2155d3351
melpon <shigemasa7watanabe+github@gmail.com>
2021-04-18 01:51:47 +0900

    ocaml 追加

---
+++ ga-build/ocaml/run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+BASE_DIR=$(cd $(dirname $0); pwd)
+cd $BASE_DIR
+
+. ../init-common.sh
+
+if [ "$SUBCOMMAND" == "setup" ]; then
+  sudo apt-get install -y \
+    unzip
+  exit 0
+fi
+
+# get sources
+
+VERSION_SHORT=${VERSION:0:4}
+
+wget_strict_sha256 \
+  http://caml.inria.fr/pub/distrib/ocaml-$VERSION_SHORT/ocaml-$VERSION.tar.gz \
+  $BASE_DIR/resources/ocaml-$VERSION.tar.gz.sha256
+
+tar xf ocaml-$VERSION.tar.gz
+
+pushd ocaml-$VERSION
+  # build
+  ./configure -prefix $PREFIX
+  make -j`nproc` world.opt
+  make install
+
+  # install opam
+  wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh
+  sh opam_installer.sh $PREFIX/bin
+
+  export PATH=$PREFIX/bin:$PATH
+  export OPAMROOT=$PREFIX/.opam
+  opam init < /dev/null
+
+  # ocamlfind
+  function install_ocaml_find_by_source {
+    git clone --depth 1 https://github.com/ocaml/ocamlfind.git
+    pushd ocamlfind
+      ./configure -bindir $PREFIX/bin
+      make -j`nproc` all
+      make install
+    popd
+  }
+
+  opam install -y ocamlfind || install_ocaml_find_by_source
+
+  # Jane Street Core を入れようとしたけど、
+  # うまくいかないので今はコメントアウトしておく
+  ## janestreet core
+  #opam install -y core
+popd
+
+# ocaml settings
+cp $BASE_DIR/resources/with-env.sh.in $PREFIX/bin/with-env.sh
+sed -i "s#@prefix@#$PREFIX#g" $PREFIX/bin/with-env.sh
+chmod +x $PREFIX/bin/with-env.sh
+
+archive_install $PREFIX $PACKAGE_PATH $PACKAGE_FILENAME
