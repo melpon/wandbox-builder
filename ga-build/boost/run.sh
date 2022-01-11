1e0307df8b23b625523ef6378eeef2d4e534c874
melpon <shigemasa7watanabe+github@gmail.com>
2022-01-11 18:46:05 +0900

    修正

--- ga-build/boost/run.sh
+++ ga-build/boost/run.sh
@@ -62,11 +62,7 @@ cd ~/
 mkdir boost-$VERSION-$COMPILER-$COMPILER_VERSION
 cd boost-$VERSION-$COMPILER-$COMPILER_VERSION
 
-if compare_version $VERSION ">=" "1.65.0"; then
-  URL=https://dl.bintray.com/boostorg/release/$VERSION/source/boost_$VERSION_TARNAME.tar.gz
-else
-  URL=http://downloads.sourceforge.net/project/boost/boost/$VERSION/boost_$VERSION_TARNAME.tar.gz
-fi
+URL=https://boostorg.jfrog.io/artifactory/main/release/$VERSION/source/boost_$VERSION_TARNAME.tar.gz
 
 curl_strict_sha256 \
   $URL \
