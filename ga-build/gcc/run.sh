02293e7a11fa52197f6069f987dde851ef8ea843
melpon <shigemasa7watanabe+github@gmail.com>
2023-11-24 11:30:00 +0900

    接続先修正

--- ga-build/gcc/run.sh
+++ ga-build/gcc/run.sh
@@ -37,8 +37,9 @@ pushd ~/tmp/gcc-$VERSION/
     tar xf gcc-$VERSION.tar.bz2
   else
     # http://ftp.tsukuba.wide.ad.jp/software/gcc/releases/gcc-$VERSION/gcc-$VERSION.tar.gz \
+    # https://bigsearcher.com/mirrors/gcc/releases/gcc-$VERSION/gcc-$VERSION.tar.gz \
     curl_strict_sha256 \
-      https://bigsearcher.com/mirrors/gcc/releases/gcc-$VERSION/gcc-$VERSION.tar.gz \
+      https://gcc.gnu.org/pub/gcc/releases/gcc-$VERSION/gcc-$VERSION.tar.gz \
       $BASE_DIR/resources/gcc-$VERSION.tar.gz.sha256
     tar xf gcc-$VERSION.tar.gz
   fi
