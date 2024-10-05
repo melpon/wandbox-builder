982345f7d3f648d41a1441cda7311054099f9b18
melpon <shigemasa7watanabe+github@gmail.com>
2024-10-05 18:15:14 +0900

    修正したりバージョンを追加したり

--- ga-build/swift/run.sh
+++ ga-build/swift/run.sh
@@ -5,20 +5,32 @@ cd $BASE_DIR
 
 . ../init-common.sh
 
+BOOTSTRAP_SWIFT_VERSION="5.10.1"
+
 if [ "$SUBCOMMAND" == "setup" ]; then
-  # https://github.com/apple/swift/blob/main/docs/HowToGuides/GettingStarted.md#ubuntu-linux より
+  # https://github.com/apple/swift/blob/main/docs/HowToGuides/GettingStarted.md#ubuntu-linux
+  # https://www.swift.org/install/linux/tarball/
+  # より
   sudo apt-get install -y \
+    binutils              \
     clang                 \
     cmake                 \
     git                   \
+    gnupg2                \
     icu-devtools          \
+    libc6-dev             \
     libcurl4-openssl-dev  \
     libedit-dev           \
+    libedit2              \
+    libgcc-13-dev         \
     libicu-dev            \
-    libncurses5-dev       \
+    libncurses-dev        \
+    libpython3-dev        \
     libpython3-dev        \
     libsqlite3-dev        \
+    libstdc++-13-dev      \
     libxml2-dev           \
+    libz3-dev             \
     ninja-build           \
     pkg-config            \
     python3               \
@@ -26,9 +38,20 @@ if [ "$SUBCOMMAND" == "setup" ]; then
     swig                  \
     systemtap-sdt-dev     \
     tzdata                \
-    uuid-dev
+    unzip                 \
+    uuid-dev              \
+    zlib1g-dev
+
+  # ブートストラップ用の Swift
+  curl_strict_sha256 \
+    https://download.swift.org/swift-${BOOTSTRAP_SWIFT_VERSION}-release/ubuntu2404/swift-${BOOTSTRAP_SWIFT_VERSION}-RELEASE/swift-${BOOTSTRAP_SWIFT_VERSION}-RELEASE-ubuntu24.04.tar.gz \
+    $BASE_DIR/resources/swift-${BOOTSTRAP_SWIFT_VERSION}-RELEASE-ubuntu24.04.tar.gz
+  tar xf swift-${BOOTSTRAP_SWIFT_VERSION}-RELEASE-ubuntu24.04.tar.gz
+
   exit 0
 fi
+
+export PATH=`pwd`/swift-${BOOTSTRAP_SWIFT_VERSION}-RELEASE-ubuntu24.04/usr/bin:$PATH
 
 git clone https://github.com/apple/swift.git
 pushd swift
@@ -43,11 +66,11 @@ pushd swift
     --skip-ios \
     --skip-watchos \
     --skip-tvos \
+    --swift-darwin-supported-archs=`uname -m` \
     --install-swift \
     --install-lldb \
-    --swift-darwin-supported-archs=`uname -m` \
     --install-destdir=$PREFIX \
-    --install-prefix=$PREFIX \
+    --install-prefix=$PREFIX
 popd
 
 archive_install $PREFIX $PACKAGE_PATH $PACKAGE_FILENAME
